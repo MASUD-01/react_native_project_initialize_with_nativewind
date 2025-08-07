32f73fb33b376415137bb833115a6f2eaf5b43f8
unknown <masudrana96141@gmail.com>
2025-08-07 22:30:26 +0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import "./global.css";
+export default function Index() {
+  return (
+    <View>
+      <Text className="bg-red-700 text-3xl text-red-400">ne app</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx}", // include if you use App.tsx at root
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
